c129f70dfdde97ce227b0388dcc1005b71caddb1
gstenger98 <gstenger98@gmail.com>
2018-02-19 00:00:02 -0800

    i won

--- favorite_movies.py
+++ favorite_movies.py
@@ -1,5 +1,9 @@
 import requests, bs4
 response = requests.get('http://www.imdb.com/user/ur53580546/ratings?sort=your_rating,desc&ratingFilter=0&mode=detail&ref_=undefined&lastPosition=0#void')
 soup = bs4.BeautifulSoup(response.text)
+movies = []
+file = open("top25.txt", "a")
 for i in range(25):
-	print(soup.find_all("h3", class_="lister-item-header")[i].select('a')[0].getText())
+	movies.append(soup.find_all("h3", class_="lister-item-header")[i].select('a')[0].getText() + "\n")
+file.writelines(movies)
+file.close()
